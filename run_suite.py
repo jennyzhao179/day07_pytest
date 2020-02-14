3f222c092bbc3d98c6de529ac7796fd26a974ed2
jennyzhao179 <59948350+jennyzhao179@users.noreply.github.com>
2020-02-14 20:02:21 +0800

    Delete run_suite.py

--- run_suite.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from tools.HTMLTestRunner import HTMLTestRunner
-
-suite = unittest.defaultTestLoader.discover("./scripts", pattern="test*.py")
-with open("./report/ihrm_report.html", "wb") as f:
-    HTMLTestRunner(stream=f).run(suite)
